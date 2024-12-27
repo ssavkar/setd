43922a192bca98a5d7e5af35506cc4f46112be49
ssavkar <savkar@inthespace.com>
2024-12-27 04:41:20 +0100

    Create setd.sh
    Setd version 0.1 initial script for setting marks in directories to give unique identifiers and then use such persistent marks to change directories including as a root to a longer path if there are sub folders.

---
+++ setd.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Copyright 2024, Sunil William Savkar <savkar@inthespace.com>
+# License: MIT License
+#
+# setd.sh - directory marks and change directory integration
+#
+
+function markstore() {
+    declare -x | grep MARKDIR_ > ~/.bash_markstore
+}
+
+function unmark {
+    unset MARKDIR_$1
+    markstore
+}
+
+function marklist {
+    { echo -e "MARK\tPATHNAME"; echo -e "====\t========"; declare -x | grep MARKDIR_ | cut -d "_" -f2- | sed 's/=/\t/g;s/\"//g'; } | column -t
+}
+
+function mark {
+    if [[ "$1" =~ ^[[:alnum:]]+$ ]]; then
+	export MARKDIR_$1=`pwd`
+	markstore
+    elif [ -z "$1" ]; then
+	marklist
+    else
+	case $1 in
+	    "-l" | "-list")
+		marklist
+		;;
+    	    "-rm" | "-remove" | "=r")
+		unmark $2
+		;;
+    	    "-h" | "-help")
+		echo "mark: usage: mark [-l|rm] mark[/additional path]"	
+		;;
+	    *) echo "Unkown option $1"
+		;;
+	esac
+    fi
+}
+
+function setd() {
+    mark=$(echo "$1" | cut -f1 -d"/")
+    remainder=$(echo "$@" | cut -d/ -f2-)
+    if [ -v 'MARKDIR_'$mark ]; then
+	if [ "$mark" != "$remainder" ]; then
+	   cddir="builtin pushd \$MARKDIR_$mark/$remainder"
+        else
+	   cddir="builtin pushd \$MARKDIR_$mark"
+	fi
+	eval $cddir > /dev/null
+    elif [ -z "$1" ]; then
+	builtin pushd ~ > /dev/null
+    else
+	case $1 in
+	    "-1")
+		builtin pushd > /dev/null
+		;;
+    	    "-l" | "-list")
+		dirs -v
+		;;
+	    *) builtin pushd $1 > /dev/null
+		;;
+	esac
+    fi
+}
+
+function cd() {
+    setd $1
+}
+
+# Source mark directory environment variables
+
+if [ -f ~/.bash_markstore ]; then
+    . ~/.bash_markstore
+fi
